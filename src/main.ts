05f71dd0f68a7ff6763759fe02804682c04339d6
Atif-Bashir-1998 <adobe8832289@gmail.com>
2022-07-16 13:25:10 +0500

    modules registered in the vue app

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import authModule from "./modules/auth";
+import blogModule from "./modules/blogs";
+
+import { registerModules } from "./register-modules";
+
+registerModules({
+  auth: authModule,
+  blog: blogModule
+});
+
 createApp(App).use(store).use(router).mount('#app')
