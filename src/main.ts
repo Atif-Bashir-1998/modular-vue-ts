761a48c9dd13265efa6e8d8cdc262139eba06571
Atif-Bashir-1998 <adobe8832289@gmail.com>
2022-07-16 13:51:13 +0500

    comments added

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// import all the modules from ./modules
 import authModule from "./modules/auth";
 import blogModule from "./modules/blogs";
 
 import { registerModules } from "./register-modules";
 
+// register the imported modules
 registerModules({
   auth: authModule,
   blog: blogModule
